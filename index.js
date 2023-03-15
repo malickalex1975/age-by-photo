603f0cd096a21be16fa94c3bf3bae8256d89a3f1
malickalex1975 <91604689+malickalex1975@users.noreply.github.com>
2023-03-16 01:09:25 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,50 +1,43 @@
 let constraints = {
   video: {
+    width: { min: 1024, ideal: 1280, max: 1920 },
+    height: { min: 720, ideal: 720, max: 1080 },
     facingMode: { exact: "user" },
   },
 };
 let streamStarted = false;
-let errors=0;
+let errors = 0;
 
 class AgeRecognition {
   constructor() {}
 
   handleStream = (stream) => {
     video.srcObject = stream;
-    console.log(video.srcObject)
+    console.log(video.srcObject);
     streamStarted = true;
-    video.play()
+    video.play();
   };
 
   startStream = async (constraints) => {
     try {
-        this.showWarning(false);
-    startScan();
-
+      this.showWarning(false);
       const stream = await navigator.mediaDevices.getUserMedia(constraints);
       this.handleStream(stream);
     } catch {
-        stopScan()
-        if(errors===0){
-      console.log("use front camera instead rear camera");
-      this.startStream({ video: { facingMode: "user" } });
-      setTimeout(() => (buttonCameraMode.textContent = "front"), 1000);
-      errors++;
-        }else{
-            console.log('allow to use camera for your browser!');
-            video.srcObject=null;
-            ageRecognition.showWarning('Ваше устройство не имеет камеру или вы запретили её использовать!',true);
-            setTimeout(()=>ageRecognition.showWarning(false),10000)
-        }
+      stopScan();
+      video.srcObject = null;
+      ageRecognition.showWarning(
+        "Ваше устройство не имеет камеру или вы запретили её использовать!",
+        true
+      );
+      setTimeout(() => ageRecognition.showWarning(false), 10000);
     }
   };
-showWarning(message='no message',isVisible){
-    let visibility=isVisible?'visible':'hidden';
-    warning.style.visibility=visibility;
-    warning.textContent=message
-
-
-}
+  showWarning(message = "no message", isVisible) {
+    let visibility = isVisible ? "visible" : "hidden";
+    warning.style.visibility = visibility;
+    warning.textContent = message;
+  }
   async play() {
     if (streamStarted) {
       video.play();
@@ -58,6 +51,9 @@ showWarning(message='no message',isVisible){
   }
 
   doScreenshot = () => {
+    navigator.vibrate(150);
+    startScan();
+    buttonAge.style.visibility='hidden'
     if (this.streamStarted) {
       canvas.width = video?.videoWidth / 2;
       canvas.height = video?.videoHeight / 2;
@@ -66,51 +62,32 @@ showWarning(message='no message',isVisible){
     }
   };
 }
-const warning=document.querySelector(".warning")
-const frame=document.querySelector(".frame")
-const scanLine=document.querySelector(".scan-line")
+const ageRecognition = new AgeRecognition();
+const warning = document.querySelector(".warning");
+const frame = document.querySelector(".frame");
+const scanLine = document.querySelector(".scan-line");
 const video = document.querySelector("video");
-const buttonCameraMode = document.querySelector(".button-camera-mode");
-buttonCameraMode.textContent = "front";
-buttonCameraMode.addEventListener("click", changeCameraMode);
+const buttonAge = document.querySelector(".button-age");
+buttonAge.textContent = "УЗНАЙ СВОЙ ВОЗРАСТ";
+buttonAge.addEventListener("click", ageRecognition.doScreenshot);
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 document.addEventListener("DOMContentLoaded", () => init());
-const ageRecognition = new AgeRecognition();
 
 function init() {
   blockScreen();
-  alert("hello");
-  ageRecognition.showWarning('Разрешите испольовать камеру в браузере!',true)
+  stopScan();
+  ageRecognition.showWarning("Разрешите использовать камеру в браузере!", true);
   ageRecognition.play();
 }
-function changeCameraMode() {
-  navigator.vibrate(50);
-  
 
-  constraints =
-    buttonCameraMode.textContent === "front"
-      ? {
-          video: {
-            facingMode: { exact: "environment" },
-          },
-        }
-      : {
-          video: {
-            facingMode: { exact: "user" },
-          },
-        };
-  buttonCameraMode.textContent =
-    buttonCameraMode.textContent === "front" ? "back" : "front";
-  video.pause();
-  streamStarted = false;
-  ageRecognition.play();
-}
 function blockScreen() {
-  screen.orientation.lock("portrait").then(console.log).catch(console.log);
+  screen.orientation.lock("portrait").then(alert).catch(alert);
 }
 
-function startScan(){scanLine.style.visibility='visible'}
-function stopScan(){
-    scanLine.style.visibility='hidden'
+function startScan() {
+  scanLine.style.visibility = "visible";
+}
+function stopScan() {
+  scanLine.style.visibility = "hidden";
 }
