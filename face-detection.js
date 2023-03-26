b1fecf19ea8da20854cd9419332473c31cff435a
Alexander Malchevsky <malickalex1975@gmail.com>
2023-03-26 17:40:57 +0300

    feat: finish project

---
+++ face-detection.js
@@ -0,0 +1,26 @@
+export class FaceDetection {
+    constructor() {}
+  
+    checkFD() {
+      if ("FaceDetector" in self) {
+       
+        return true;
+      } else {
+        console.log("FaceDetector is not supported");
+        return false;
+      }
+    }
+    detect(theImage) {
+      try {
+        let faceDetector = new FaceDetector({
+          fastMode: true,
+          maxDetectedFaces: 1,
+        });
+  
+        let detectedFaces = faceDetector.detect(theImage);
+        return detectedFaces;
+      } catch (err) {
+        console.error("Face Detection failed, boo.", err);
+      }
+    }
+}
